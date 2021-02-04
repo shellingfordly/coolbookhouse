22d56c8733c148d524fbc955cbabd7edd1852e72
shellingfordly <ly@192.168.1.8>
2021-02-04 22:46:29 +0800

    style: modify table style

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,9 +1,9 @@
 import http from './http'
 
 export function getBookList(){
-  return http('http://localhost:8080/book/get.list.1.0.0')
-}s
+  return http('http://182.61.19.153:8080/book/get.list.1.0.0')
+}
 
 export function addBook(params){
-  return http('http://localhost:8080/book/add.1.0.0', params, 'post') 
+  return http('http://182.61.19.153:8080/book/add.1.0.0', params, 'post') 
 }
--- web/src/views/home/components/HomeTable.vue
+++ web/src/views/home/components/HomeTable.vue
@@ -1,10 +1,16 @@
 <template>
   <el-table :data="tableData" style="width: 100%" size="small">
-    <el-table-column prop="name" label="书名" width="180" />
-    <el-table-column prop="link" label="下载地址" />
-    <el-table-column prop="uppsw" label="提取码" />
-    <el-table-column prop="gzpsw" label="解压密码" />
-    <el-table-column prop="user" label="提供者" />
+    <el-table-column prop="name" label="书名" />
+    <el-table-column label="下载地址" width="120px">
+      <template #default="scope">
+        <a  class="book-link" :href="scope.row.link" target="brank">
+          <i class="el-icon-link"></i>下载链接
+        </a>
+      </template>
+    </el-table-column>
+    <el-table-column prop="uppsw" label="提取码" width="120px" />
+    <el-table-column prop="gzpsw" label="解压密码" width="120px" />
+    <el-table-column prop="user" label="提供者" width="120px" />
   </el-table>
 </template>
 <script>
@@ -18,8 +24,14 @@ export default defineComponent({
     },
   },
   setup() {
-   
     return {};
   },
 });
 </script>
+
+<style lang="scss" scoped>
+@import '../../../style/color';
+.book-link {
+  color: $link-font-color;
+}
+</style>
