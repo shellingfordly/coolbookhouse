6799b0effa6c031987e7c9f7ed37190109e82ba8
shellingfordly <shellingfordly@qq.com>
2021-11-22 03:33:48 +0800

    feat: update dist

--- web/src/api/url.js
+++ web/src/api/url.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://182.61.19.153:8080'
+const BASE_URL = '/api'
 
 const urls = {
   getBookListUrl: '/book/get.list.1.0.0',
@@ -13,7 +13,6 @@ function getUrl() {
   for (const key in urls) {
     if (Reflect.has(urls, key)) {
       const url = urls[key];
-      console.log(key);
       res[key] = BASE_URL + url
     }
   }
